10d75cedfd94b7926c8fbe6b817bcb5c18076c27
alejopm03 <perdomomilar@gmail.com>
2024-11-27 19:37:23 -0300

    feat(uart): implement rx uart parser

--- firmware/load/Core/Inc/uart.h
+++ firmware/load/Core/Inc/uart.h
@@ -1,9 +1,10 @@
 #ifndef UART_H
 #define UART_H
 
+#include <mcp4725.h>
 #include "stm32f1xx_hal.h"
 
-void uart_init(UART_HandleTypeDef *huart_rx);
+void uart_init(UART_HandleTypeDef *huart_rx, mcp4725_t *dac_handler);
 
 void uart_run(void);
 
