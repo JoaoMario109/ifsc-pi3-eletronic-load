9bed7ff2efdffc1fa47213e77e7bacd2390eedb3
alejopm03 <perdomomilar@gmail.com>
2024-12-04 12:42:34 -0300

    feat(load): add constant voltage control

--- firmware/load/Core/Src/control.c
+++ firmware/load/Core/Src/control.c
@@ -7,6 +7,9 @@
 #define MAX_CURRENT 10.0f
 #define MIN_CURRENT 0.0f
 
+#define MAX_VOLTAGE 3.3f
+#define MIN_VOLTAGE 0.0f
+
 /* Anti windup */
 #define MAX_INTEGRAL 3.0f
 #define MIN_INTEGRAL -1.0f
@@ -16,11 +19,18 @@
 
 extern mcp4725_t *dac;
 
+typedef enum
+{
+	CONTROL_MODE_CURRENT,
+	CONTROL_MODE_VOLTAGE
+} control_mode_t;
+
 /**
  * @brief pid controller main structure
- * 
+ *
  */
-typedef struct {
+typedef struct
+{
 	float kp; // Proportional gain
 	float ki; // Integral gain
 	float kd; // Derivative gain
@@ -31,55 +41,63 @@ typedef struct {
 
 	double error_history[3];  // Store last error values for derivative calculation
 	double output_history[3]; // Store last output values for derivative calculation
-	double integral;          // Integral term
-	int history_index;        // Index for storing error history
+	double integral;		  // Integral term
+	int history_index;		  // Index for storing error history
 
 } pid_controller_t;
 
 /**
  * @brief Controller i/o
- * 
+ *
  */
-typedef struct {
+typedef struct
+{
 	double setpoint;
 	double measured_value;
 	double control_action;
 } control_io_t;
 
 /* Static variables */
-static control_io_t control_io = {
+static control_io_t current_control_io = {
 	.setpoint = 1.5f,
 	.measured_value = 0.0f,
-	.control_action = 0.0f
+	.control_action = 0.0f};
 
-};
-static pid_controller_t pid_controller;
+static control_io_t voltage_control_io = {
+	.setpoint = 2.5f,
+	.measured_value = 0.0f,
+	.control_action = 0.0f};
+
+static pid_controller_t current_pid_controller;
+static pid_controller_t voltage_pid_controller;
+static control_mode_t current_mode = CONTROL_MODE_CURRENT;
 
 /* Static function prototypes */
 static void pid_init(pid_controller_t *pid, double ku, double tu, double control_frequency, double filter_coeff);
 static double pid_update(pid_controller_t *pid, double setpoint, double measured_value);
 
-
 /**
  * @brief initialize the control module
- * 
+ *
  * @return esp_err_t error code
  */
 HAL_StatusTypeDef control_init(void)
 {
-	
-	/* Initialize pid controller */
+	/* Initialize current PID controller */
 	pid_init(
-		/* PID controller instance */
-		&pid_controller,
-		/* Ultimate gain */
+		&current_pid_controller,
 		5.0,
-		/* Ultimate period */
 		0.41,
-		/* Control frequency */
 		CONTROL_FREQUENCY,
-		/* Filter coefficient */
-		5.0); 
+		5.0);
+
+	/* Initialize voltage PID controller */
+	pid_init(
+		&voltage_pid_controller,
+		5.0,
+		0.41,
+		CONTROL_FREQUENCY,
+		5.0);
 
 	return HAL_OK;
 }
@@ -94,60 +112,20 @@ HAL_StatusTypeDef control_init(void)
  * @param filter_coeff Filter coefficient
  * @return * Function returns void
  */
-void pid_init(pid_controller_t *pid, double ku, double tu, double control_frequency, double filter_coeff) {
-	/* Initialize PID controller gains */
+void pid_init(pid_controller_t *pid, double ku, double tu, double control_frequency, double filter_coeff)
+{
 	pid->kp = 0.2;
 	pid->ki = 74.0;
 	pid->kd = 0.00062;
-
-	/* Initialize control frequency */
 	pid->control_frequency = control_frequency;
-
-	/* Initialize error histories */
-	for (int i = 0; i < (sizeof(pid->error_history) / sizeof(pid->error_history[0])); ++i) {
+	pid->integral = 0.0;
+	for (int i = 0; i < 3; ++i)
+	{
 		pid->error_history[i] = 0.0;
-	}
-
-	/* Initialize output histories */
-	for (int i = 0; i < (sizeof(pid->output_history) / sizeof(pid->output_history[0])); ++i) {
 		pid->output_history[i] = 0.0;
 	}
-
-	/* Initialize history index */
 	pid->history_index = 0;
-
-	pid->integral = 0.0;
-
 }
-
-void pid_reset(pid_controller_t *pid) 
-{
-	/* Initialize error histories */
-	for (int i = 0; i < (sizeof(pid->error_history) / sizeof(pid->error_history[0])); ++i) {
-		pid->error_history[i] = 0.0;
-	}
-
-	/* Initialize output histories */
-	for (int i = 0; i < (sizeof(pid->output_history) / sizeof(pid->output_history[0])); ++i) {
-		pid->output_history[i] = 0.0;
-	}
-
-	/* Initialize history index */
-	pid->history_index = 0;
-
-	pid->integral = 0.0;
-}
-
-void pid_set_constants(double kp, double ki, double kd) 
-{
-	LOG_INFO("Setting PID constants: %f, %f, %f\n", kp, ki, kd);
-	pid_controller.kp = kp;
-	pid_controller.ki = ki;
-	pid_controller.kd = kd;
-
-	pid_reset(&pid_controller);
-}
-
 
 /**
  * @brief PID controller update function
@@ -158,38 +136,24 @@ void pid_set_constants(double kp, double ki, double kd)
  * @param measured_value controller measured value
  * @return double controller output
  */
-double pid_update(pid_controller_t *pid, double setpoint, double measured_value) {
-	const float kp = pid->kp;
-	const float ki = pid->ki;
-	//const float Kd = 0;
-	const float kd = pid->kd;
-
+double pid_update(pid_controller_t *pid, double setpoint, double measured_value)
+{
 	const double error = setpoint - measured_value;
+	const double proportional = pid->kp * error;
 
-	const double proportional = kp * error;
-
-	pid->integral += ki * ((pid->error_history[0] + error) / 2.0) * (1.0 / pid->control_frequency);
-
-	const double derivative = kd * (2.0 * error - pid->error_history[0] - pid->error_history[1]);
-
-	/* Anti windup */
+	pid->integral += pid->ki * ((pid->error_history[0] + error) / 2.0) * (1.0 / pid->control_frequency);
 	if (pid->integral > MAX_INTEGRAL)
 		pid->integral = MAX_INTEGRAL;
-
 	else if (pid->integral < MIN_INTEGRAL)
 		pid->integral = MIN_INTEGRAL;
 
+	const double derivative = pid->kd * (2.0 * error - pid->error_history[0] - pid->error_history[1]);
 
-	double output = proportional + pid->integral + derivative;
-
-	/* Store error history */
 	pid->error_history[2] = pid->error_history[1];
 	pid->error_history[1] = pid->error_history[0];
 	pid->error_history[0] = error;
 
-	pid->output_history[0] = proportional;
-
-	/* Anti windup */
+	double output = proportional + pid->integral + derivative;
 	if (output > MAX_DAC_VOLTAGE)
 		output = MAX_DAC_VOLTAGE;
 	else if (output < MIN_DAC_VOLTAGE)
@@ -199,49 +163,54 @@ double pid_update(pid_controller_t *pid, double setpoint, double measured_value)
 }
 
 /**
- * @brief Update the control action
- * 
+ * @brief Update the control action based on current mode
  */
 void control_update(void)
-
 {
-	control_io.measured_value = adc_get_value(ADC_INPUT_CURRENT);
-	control_io.control_action = pid_update(&pid_controller, control_io.setpoint, control_io.measured_value);
-
-	mcp4725_set_voltage(dac, 3.3, control_io.control_action, false);
-
+	if (current_mode == CONTROL_MODE_CURRENT)
+	{
+		current_control_io.measured_value = adc_get_value(ADC_INPUT_CURRENT);
+		current_control_io.control_action = pid_update(&current_pid_controller, current_control_io.setpoint, current_control_io.measured_value);
+		mcp4725_set_voltage(dac, 3.3, current_control_io.control_action, false);
+	}
+	else if (current_mode == CONTROL_MODE_VOLTAGE)
+	{
+		voltage_control_io.measured_value = adc_get_value(ADC_INPUT_VOLTAGE);
+		voltage_control_io.control_action = pid_update(&voltage_pid_controller, voltage_control_io.setpoint, voltage_control_io.measured_value);
+		mcp4725_set_voltage(dac, 3.3, voltage_control_io.control_action, false);
+	}
 }
 
 /**
- * @brief setter for the current setpoint
- * 
- * @param setpoint setpoint to set
+ * @brief Setter for the current mode
+ *
+ * @param mode CONTROL_MODE_CURRENT or CONTROL_MODE_VOLTAGE
  */
-void control_set_current_setpoint(float setpoint)
+void control_set_mode(control_mode_t mode)
 {
-	control_io.setpoint = setpoint;
-
-	if (setpoint > MAX_CURRENT)
-	{
-		control_io.setpoint = MAX_CURRENT;
-	}
-	else if (setpoint < MIN_CURRENT)
-	{
-		control_io.setpoint = MIN_CURRENT;
-	}
+	current_mode = mode;
 }
 
-float control_get_current_setpoint(void)
+/**
+ * @brief Setter for the voltage setpoint
+ *
+ * @param setpoint Voltage setpoint to set
+ */
+void control_set_voltage_setpoint(float setpoint)
 {
-	return control_io.setpoint;
+	voltage_control_io.setpoint = setpoint;
+	if (setpoint > MAX_VOLTAGE)
+		voltage_control_io.setpoint = MAX_VOLTAGE;
+	else if (setpoint < MIN_VOLTAGE)
+		voltage_control_io.setpoint = MIN_VOLTAGE;
 }
 
-float control_get_current_control_action(void)
+float control_get_voltage_setpoint(void)
 {
-	return control_io.control_action;
+	return voltage_control_io.setpoint;
 }
 
-float control_get_current_measured_value(void)
+float control_get_voltage_measured_value(void)
 {
-	return control_io.measured_value;
+	return voltage_control_io.measured_value;
 }
